0cf924453e0ae3a939ed94e388efe9103cd2e559
Ahmad Dzaky Hafidz M <35956014+seedev43@users.noreply.github.com>
2023-06-06 02:15:39 +0700

    update Button Menu

--- system/cmd/main/button.js
+++ system/cmd/main/button.js
@@ -1,21 +1,20 @@
+import { InlineKeyboard } from "grammy"
+
 export default {
   name: "button",
   cmd: ["button"],
   tags: "main",
   desc: "buat button",
   run: async ({ ctx, m }) => {
-    let arr = {
-      inline_keyboard: [
-        [
-          { text: "Satu", url: "https://google.com" },
-          { text: "Dua", url: "https://github.com" },
-        ],
-        [{ text: "Callback button", callback_data: "oke" }],
-      ],
-    };
+    const inlineKeyboard = new InlineKeyboard()
+      .url("Github", "https://github.com/seedev4")
+      .url("Facebook", "https://www.facebook.com/khunbotme").row()
+      .text("Callback button", "oke")
+
+    m.reply("INI MENU BUTTON", { reply_markup: inlineKeyboard })
 
     ctx.api.sendMessage(m.chatid, "Ini button", {
-      reply_markup: arr,
+      reply_markup: inlineKeyboard,
     });
   },
 };
