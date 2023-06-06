cce9793621803daac236a4c21b2a52dd44707d18
Ahmad Dzaky Hafidz M <35956014+seedev43@users.noreply.github.com>
2023-06-06 10:17:55 +0700

    Update respon button

--- system/event/answerCallbackQuery.js
+++ system/event/answerCallbackQuery.js
@@ -1,3 +1,5 @@
+import { InlineKeyboard } from "grammy";
+
 export const answerCallbackQuery = async (ctx, m) => {
   if (m.update?.callback_query) {
     m.cb = m.update?.callback_query;
@@ -7,15 +9,8 @@ export const answerCallbackQuery = async (ctx, m) => {
 
     if (m.cbdata == "oke") {
       ctx.api.editMessageText(m.chatid, m.msgid, "edit pesan button", {
-        reply_markup: {
-          inline_keyboard: [
-            [
-              { text: "Button 1", callback_data: "btn1" },
-              { text: "Button 2", callback_data: "btn2" },
-            ],
-            [{ text: "Button 3", callback_data: "btn3" }],
-          ],
-        },
+        reply_markup: new InlineKeyboard()
+          .text("Button 1", "btn1")
       });
     }
   }
